1a7963821f334d18fc1214deafa725ebe550cae7
Kisara Beddawala <kisarabeddawala@gmail.com>
2025-07-18 21:51:48 +0530

    validate signup page using js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,126 @@
-//login page valid 
+//login page valid
 
-function showMessage_Login(input,message,type){
-    const msg = input.parentNode.querySelector(".small-login");
-    msg.innerText = message;
-    input.className = type ? "success-login" : "error-login";
-    return type;
+function showMessage_Login(input, message, type) {
+  const msg_login = input.parentNode.querySelector(".small-login");
+  msg_login.innerText = message;
+  input.className = type ? "success-login" : "error-login";
+  return type;
 }
 
-function showError_Login(input,message){
-    return showMessage_Login(input,message,false);
+function showError_Login(input, message) {
+  return showMessage_Login(input, message, false);
 }
-function showSuccess_Login(input){
-    return showMessage_Login(input,"",true);
+function showSuccess_Login(input) {
+  return showMessage_Login(input, "", true);
 }
 
-function hasValue_Login(input,message){
-    if(input.value.trim() === ""){
-        return showError_Login(input,message);
-    }
-    return showSuccess_Login(input);
+function hasValue_Login(input, message) {
+  if (input.value.trim() === "") {
+    return showError_Login(input, message);
+  }
+  return showSuccess_Login(input);
 }
 
 const login_form = document.getElementById("login-form");
 const EMAIL_REQUIRED_LOGIN = "Please Enter Your Email";
 const PASSWORD_REQUIRED_LOGIN = "Please Enter Your Password";
 
-if(login_form){
-    login_form.addEventListener("submit",function(event){
-        event.preventDefault();
+if (login_form) {
+  login_form.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-        const emailvalid = hasValue_Login(login_form.elements["login-email"],EMAIL_REQUIRED_LOGIN);
-        const passwordvalid  = hasValue_Login(login_form.elements["login-password"],PASSWORD_REQUIRED_LOGIN);
+    const emailvalid_login = hasValue_Login(
+      login_form.elements["login-email"],
+      EMAIL_REQUIRED_LOGIN
+    );
+    const passwordvalid_login = hasValue_Login(
+      login_form.elements["login-password"],
+      PASSWORD_REQUIRED_LOGIN
+    );
 
-        if(emailvalid && passwordvalid){
-            alert("Login Successfully");
-        }
-    })
+    if (emailvalid && passwordvalid) {
+      alert("Login Successfully");
+    }
+  });
+}
+
+//signup page valid
+function showMessage_Signup(input, message, type) {
+  const msg_signup = input.parentNode.querySelector(".small-signup");
+  msg_signup.innerText = message;
+  input.className = type ? "success-signup" : "error-signup";
+  return type;
+}
+
+function showError_Signup(input, message) {
+  return showMessage_Signup(input, message, false);
+}
+
+function showSuccess_Signup(input) {
+  return showMessage_Signup(input, "", true);
+}
+
+function hasValue_signup(input, message) {
+  if (input.value.trim() === "") {
+    return showError_Signup(input, message);
+  }
+  return showSuccess_Signup(input);
+}
+
+const signup_form = document.getElementById("signup-form");
+
+const NAME_REQUIRED_SIGNUP = "Please Enter Your Name";
+const EMAIL_REQUIRED_SIGNUP = "Please Enter Your Email";
+const ADDRESS_REQUIRED_SIGNUP = "Please Enter Your Address";
+const NUMBER_REQUIRED_SIGNUP = "Please Enter Your Phone Number";
+const PASSWORD1_REQUIRED_SIGNUP = "Please Enter Your Password";
+const PASSWORD2_REQUIRED_SIGNUP = "Confirm Your Password";
+
+if (signup_form) {
+  signup_form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const namevalid_signup = hasValue_signup(
+      signup_form.elements["signup-name"],
+      NAME_REQUIRED_SIGNUP
+    );
+    const emailvalid_signup = hasValue_signup(
+      signup_form.elements["signup-email"],
+      EMAIL_REQUIRED_SIGNUP
+    );
+    const addressvalid_signup = hasValue_signup(
+      signup_form.elements["signup-address"],
+      ADDRESS_REQUIRED_SIGNUP
+    );
+    const numbervalid_signup = hasValue_signup(
+      signup_form.elements["signup-number"],
+      NUMBER_REQUIRED_SIGNUP
+    );
+    const password1valid_signup = hasValue_signup(
+      signup_form.elements["signup-password1"],
+      PASSWORD1_REQUIRED_SIGNUP
+    );
+    const password2valid_signup = hasValue_signup(
+      signup_form.elements["signup-password2"],
+      PASSWORD2_REQUIRED_SIGNUP
+    );
+
+    if (
+      namevalid_signup &&
+      emailvalid_signup &&
+      addressvalid_signup &&
+      numbervalid_signup &&
+      password1valid_signup &&
+      password2valid_signup
+    ) {
+      if (
+        signup_form.elements["signup-password1"].value ===
+        signup_form.elements["signup-password2"].value
+      ) {
+        alert("Register Successfully");
+      } else {
+        alert("Password doesn't Match");
+      }
+    }
+  });
 }
